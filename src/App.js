e3d622561b5f8e544e42f5bfd97fad4273230d73
mani rathna <mani.rathna@c-c-c.ca>
2022-02-18 18:26:34 +0530

    useSelector completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Header from "./containers/Header";
-import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom'
+// import {  BrowserRouter, BrowserRouter as Route, Routes,} from 'react-router-dom'
 import ProductListing from "./containers/ProductListing";
 import ProductDetail from "./containers/ProductDetatil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-      <Header />
-     
-      <Route path="/" exact component={ProductListing} />
-     <Route path="/product/:productId" exactcomponent={ProductDetail} />
-     <Route>404 not found</Route>
-      
-    
-      </Routes>
-     
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>} />
+          <Route path="/product/:productId" exact element={<ProductDetail/>} />
+          <Route>404 not found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.initialState.products);
+  const {id, title} = products[0]
+ 
   return (
-    <div>
-<h1>ProductListing</h1>
+    <div className='four column wide'>
+   <div className='ui link card'>
+     <div className='card'>
+       <div className='image'>
+       </div>
+       <div className='content'>
+           <div className='header'>
+            {title}
+            {id}
+           </div>
+         </div>
+     </div>
+   </div>
     </div>
   )
 }
